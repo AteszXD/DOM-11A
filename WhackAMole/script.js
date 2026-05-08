654ef961d77e416efa025b24af5673b0ce6a9eeb
AteszXD <144881460+AteszXD@users.noreply.github.com>
2026-05-08 20:45:05 +0200

    játék indítása

--- WhackAMole/script.js
+++ WhackAMole/script.js
@@ -20,3 +20,32 @@ function randomHole() {
   hole.innerHTML = `<div class="mole">🐹</div>`;
   currentHole = hole;
 }
+
+function startGame() {
+  score = 0;
+  timeLeft = 30;
+  moleSpeed = 1000;
+
+  scoreDisplay.textContent = score;
+  timeDisplay.textContent = timeLeft;
+
+  clearInterval(gameTimer);
+  clearInterval(moleTimer);
+
+  randomHole();
+
+  moleTimer = setInterval(() => {
+    randomHole();
+  }, moleSpeed);
+
+  gameTimer = setInterval(() => {
+    timeLeft--;
+    timeDisplay.textContent = timeLeft;
+
+    if (score >= 5 && moleSpeed > 700) increaseDifficulty(700);
+    if (score >= 10 && moleSpeed > 500) increaseDifficulty(500);
+    if (score >= 15 && moleSpeed > 350) increaseDifficulty(350);
+    if (timeLeft <= 0) endGame();
+
+  }, 1000);
+}
